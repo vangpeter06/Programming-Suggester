ffac1b762459728b0d06dd51c9c21a108ccd894a
Peter Vang <vang_peter06@yahoo.com>
2022-06-03 15:53:12 -0700

    WIP finding how to output text with JS.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,29 +1,63 @@
-function aboutMeInfo(nam, year, favBeer, favSnack,favSport) {
-  return  "My name is say " + nam + "," + year + " years old. " + "My favorite beer is " favBeer "," 
-  "favorite snack is " =favSnack + "," + " and my favorite sport is " + favSport + "."
+function aboutMeInfo(beer, snack,) {
+  return beer + snack
+  // return  "My name is say " + name + "," + age + " years old. " + "My favorite beer is " favoriteBeer "," 
+  // "favorite snack is " =favoriteSnack + "," + " and my favorite sport is " + favoriteSport + "."
 }
 
-
-
 $(document).ready(function() {
-  $("button#submit").click(function(event) {
+  $("form#program").submit(function(event) {
     event.preventDefault();
-    const nameInput = $("input#name").val();
-    const ageInput = $("input#age").val();
-    // const favoriteBeer = $("select#beer").val();
-    // // const favoriteBeer = $("#beer").val();
-    // const favoriteSnack = $("select#snack").val();
-    // // const favoriteSnack = $("#snack").val();
-    // const favoriteSport = $("input:radio[name=sport]:checked").val;
-    const result = aboutMeInfo(nam, year, favBeer, favSnack, favSport);
-    $(".name").text(nameInput);
-    $(".age").text(ageInput);
+    const name = $("#name").val();
+    const age = $("#age").val();
+    const beer = parseInt($("#beer").val());
+    const snack = parseInt($("#snack").val());
+    const sport = $("input:radio[name=sport]:checked").val;
+    console.log(sport)
+    const total = aboutMeInfo(beer, snack);
+    
+    $("#username").text(name);
+    // $(#"output").show(total);
+    
+    // $("#username").text(name);
+    
+    if ((sport === "basketball")) {
+      if (total <= 10)  {
+        $("#language").text("Ruby");
+        $("#hidden").show();
+      } else if (total > 11 && total <= 20) {
+        $("#language").text("Java");
+        $("#hidden").show();
+      } else (total > 20 && total <= 40)
+        $("#language").text("C#");
+        $("#hidden").show();
+  
+    }if ((sport === "football")) {
+      if (total <= 10)  {
+        $("#language").text("Ruby");
+        $("#hidden").show();
+      } else if (total > 11 && total <= 20) {
+        $("#language").text("Java");
+        $("#hidden").show();
+      } else (total > 20 && total <= 40)
+        $("#language").text("C#");
+        $("#hidden").show();
+    
+      }if ((sport === "soccer")) {
+      if (total <= 10)  {
+        $("#language").text("Ruby");
+        $("#hidden").show();
+      } else if (total > 11 && total <= 20) {
+        $("#language").text("Java");
+        $("#hidden").show();
+      } else (total > 20 && total <= 40)
+        $("#language").text("C#");
+        $("#hidden").show();
+    }
     // $(".beer").text(beerInput);
     // $(".snack").text(snackInput);
     // $(".sport").text(sportInput);
-    $("#output").text(result);
+    // $("#output").text(result);
     // $("#output").show(result);
-
   });
 });
 

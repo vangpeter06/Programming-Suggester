8458d97ad722452c32426042b4a5514ecd585a66
Peter Vang <vang_peter06@yahoo.com>
2022-06-04 20:27:11 -0700

    add style to class in CSS and add reset button to JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,22 +1,30 @@
 function aboutMeInfo(beer, snack,) {
-  return beer + snack;
+  return beer + snack
   // return  "My name is say " + name + "," + age + " years old. " + "My favorite beer is " favoriteBeer "," 
   // "favorite snack is " =favoriteSnack + "," + " and my favorite sport is " + favoriteSport + "."
 }
 
 $(document).ready(function() {
-  $("form#program").submit(function(event) {
+  $("form#program").submit(function (event) {
     event.preventDefault();
     const name = $("#name").val();
     const age = $("#age").val();
     const beer = parseInt($("#beer").val());
     const snack = parseInt($("#snack").val());
-    const sport = $("input:radio[name=sport]:checked").val;
+    const sport = $("input:radio[name=sport]:checked").val();
     
     const total = aboutMeInfo(beer, snack);
     
+  
     $("#username").text(name);
-    console.log(.text(name));
+    
+    $(".btn-reset").click(function () {
+      $("#program").trigger("reset");
+      $("#output").hide();
+      // $("#hidden-fail").hide()
+      $(".btn-submit").show();
+    });
+    
     // $(#"output").show(total);
     
     // $("#username").text(name);
@@ -24,36 +32,36 @@ $(document).ready(function() {
     if ((sport === "basketball")) {
       if (total <= 10)  {
         $("#language").text("Ruby");
-        $("#hidden").show();
+        $("#output").show();
       } else if (total > 11 && total <= 20) {
         $("#language").text("Java");
-        $("#hidden").show();
-      } else (total > 20 && total <= 40)
+        $("#output").show();
+      } else (total > 20 && total <= 40);
         $("#language").text("C#");
-        $("#hidden").show();
+        $("#output").show();
   
     }if ((sport === "football")) {
       if (total <= 10)  {
         $("#language").text("Ruby");
-        $("#hidden").show();
+        $("#output").show();
       } else if (total > 11 && total <= 20) {
         $("#language").text("Java");
-        $("#hidden").show();
-      } else (total > 20 && total <= 40)
+        $("#output").show();
+      } else (total > 20 && total <= 40);
         $("#language").text("C#");
-        $("#hidden").show();
+        $("#").show();
     
       }if ((sport === "soccer")) {
       if (total <= 10)  {
         $("#language").text("Ruby");
-        $("#hidden").show();
+        $("#output").show();
       } else if (total > 11 && total <= 20) {
         $("#language").text("Java");
-        $("#hidden").show();
-      } else (total > 20 && total <= 40)
+        $("#output").show();
+      } else (total > 20 && total <= 40);
         $("#language").text("C#");
-        $("#hidden").show();
-    }
+        $("#output").show();
+      }
     // $(".beer").text(beerInput);
     // $(".snack").text(snackInput);
     // $(".sport").text(sportInput);
